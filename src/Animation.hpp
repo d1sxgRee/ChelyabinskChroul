dbcd43ad1b598c92cc686e1216afa3d5395df4ae
d3f4u1t <87200908+C3mka21@users.noreply.github.com>
2022-03-04 12:34:50 +0300

    Мини обнова

--- src/Animation.hpp
+++ src/Animation.hpp
@@ -7,6 +7,7 @@
 #define __ANIMATION_HPP__
 #include <TXLib.h>
 
+const int start_x = 1;
 class Animation
 {
 private:
@@ -17,20 +18,22 @@ private:
     double scaleX;   //требуемое изменение картинки на холсте по ох
     double scaleY;   //требуемое изменение картинки на холсте по оу
     double endX;     //конечная длина одного спрайтшита
+    double loop_x;   //позиция начала цикла на спрайте по х
     HDC image;       //указатель на спрайтшит
     COLORREF color;  //удаляемый цвет со спрайта
 public:
-    Animation(double _x, double _y, double _width, double _height, double _scaleX, double _scaleY, double _endX, HDC _image, COLORREF _color);
+    Animation(double _x, double _y, double _width, double _height, double _scaleX, double _scaleY, double _endX, double _loop_x, HDC _image, COLORREF _color);
     Animation(const Animation& a);
-    Animation& operator= (const Animation& a);
+    Animation& operator= (const Animation& a) = default;
     ~Animation();
     void draw(double x,double y);
     void update(double x, double y);
     void setY(double y);
+    void set_default_position();
 };
 
-Animation::Animation(double _x, double _y, double _width, double _height, double _scaleX, double _scaleY, double _endX, HDC _image, COLORREF _color):
-    sprite_x(_x), sprite_y(_y), width(_width), height(_height), scaleX(_scaleX), scaleY(_scaleY), endX(_endX),
+Animation::Animation(double _x, double _y, double _width, double _height, double _scaleX, double _scaleY, double _endX, double _loop_x, HDC _image, COLORREF _color):
+    sprite_x(_x), sprite_y(_y), width(_width), height(_height), scaleX(_scaleX), scaleY(_scaleY), endX(_endX), loop_x(_loop_x),
     image(_image), color(_color)
 {
     if(image==nullptr)
@@ -42,15 +45,15 @@ Animation::Animation(double _x, double _y, double _width, double _height, double
 
 Animation::Animation(const Animation& a) :
     sprite_x(a.sprite_x), sprite_y(a.sprite_y), width(a.width), height(a.height), scaleX(a.scaleX),
-    scaleY(a.scaleY), endX(a.endX), image(a.image), color(a.color) {}
+    scaleY(a.scaleY), endX(a.endX),loop_x(a.loop_x), image(a.image), color(a.color) {}
 
 Animation::~Animation() { txDeleteDC(image); }
 
 void Animation::draw(double x, double y)
 {
     Win32::TransparentBlt (txDC(), static_cast < int > (x), static_cast < int > (y),
-    static_cast < int > (width*scaleX), static_cast < int > (height*scaleY),
-    image, static_cast < int > (sprite_x),
+    static_cast < int > (width*scaleX), static_cast < int > (height*scaleY),image,
+    static_cast < int > (sprite_x),
     static_cast < int > (sprite_y), static_cast < int > (width),
     static_cast < int > (height), color);
 }
@@ -61,13 +64,21 @@ void Animation::update(double screen_x, double screen_y)
 {
     draw(screen_x, screen_y);
     sprite_x+= width;
-    if(sprite_x >= endX-width)
+    if(sprite_x > endX-width)
     {
-        sprite_x = 1;
+        sprite_x = loop_x;
     }
 }
 
-void Animation::setY(double y){sprite_y=y;}
+void Animation::setY(double y)
+{
+    sprite_y=y;
+}
+
+void Animation::set_default_position()
+{
+    sprite_x = 0;
+}
 
 #endif //__ANIMATION_H__
 
