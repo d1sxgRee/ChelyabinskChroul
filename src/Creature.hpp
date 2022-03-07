a032f9b0b1314456b4ff79bbc6010f9e66151481
Dark Watchman <space.arseniy@gmail.com>
2022-03-07 22:15:50 +0300

    я не доделал, простите(((, завтра всё будет, обещаю(((

--- src/Creature.hpp
+++ src/Creature.hpp
@@ -8,21 +8,39 @@
 #include "Data.hpp"
 #include "Level.hpp"
 #include <map>
+#include <fstream>
+#include <cassert>
+
+enum Command
+{
+    GRIGHT,
+    GLEFT,
+    JUMP,
+    ATTACK
+};
+
+static std::map < std::string, E > const commandTable =
+    {{"GRIGHT", Command::GRIGHT},
+     {"GLEFT", Command::GLEFT},
+     {"JUMP", Command::JUMP},
+     {"ATTACK", Command::ATTACK}};
 
 class Creature
 {
 private:
     Coords coords;
     Data data;
-    std::map < std::string, Animation > animations;
+    std::map < Command, Animation > animations;
 public:
-    Creature(Coords _coords, Data _data, std::map < std::string, Animation > _animations);
+    Creature(Coords _coords, Data _data, std::map < Command, Animation > _animations);
+    initAnimations(std::string path);
 
-    void addAnimation(std::string name, Animation animation);
+    void addAnimation(Command command, Animation animation);
+    int executeCommand(Command command); // возвращает 1, если дана некоректная комманда
     void moveRight();
     void moveLeft();
     void jump();
-    void keys_interaction(int rkey, int lkey);
+    void keys_interaction(int rkey, int lkey) = delete;
     Coords getCoords();
     Data getData();
 };
@@ -30,11 +48,37 @@ public:
 Creature::Creature(Coords _coords, Data _data, std::map < std::string, Animation > _animations) :
     coords(_coords), data(_data), animations(_animations) {}
 
-void Creature::addAnimation(std::string name, Animation animation)
+void Creature::addAnimation(Command command, Animation animation)
 {
-    if(animations.count(name) != 0)
+    if(animations.count(command) != 0)
         return;
-    animations.insert(std::make_pair(name, animation));
+    animations.insert(std::make_pair(command, animation));
+}
+
+Creature::initAnimations(std::string path)
+{
+    fistream fin (path);
+    std::string buffer;
+    fin >> buffer;
+
+}
+
+void Creature:::executeCommand(Command command)
+{
+    assert(animations.contains(command));
+    switch (command){
+      case GRIGHT:
+          this->moveRight();
+          break();
+      case GLEFT:
+          this->moveLeft();
+          break();
+      case JUMP:
+          this->jump();
+          break();
+      case ATTACK:
+          break();
+    }
 }
 
 void Creature::moveRight()
@@ -43,6 +87,7 @@ void Creature::moveRight()
     coords.y += std::abs(data.get_velocity()) / WindowR;
     data.set_velocity(std::abs(data.get_velocity()) + std::abs(data.get_acceleration()));
 }
+
 void Creature::moveLeft()
 {
     coords.x -= std::abs(data.get_velocity());
