670f729a9bf82df84feaaa108ebc1251c8565d65
Roma1000-7 <82238085+Roma1000-7@users.noreply.github.com>
2022-03-08 22:26:25 +0300

    Столкновение с платформой
    Добавлен метод (bool), проверяющий столкновение Creature с платформой.

--- src/Platform.hpp
+++ src/Platform.hpp
@@ -2,7 +2,7 @@
 #define __PLATFORM_H__
 
 #include "TXLib.h"
-#include "Coords.hpp"
+#include "Creature.hpp"
 
 class Platform
 {
@@ -17,6 +17,7 @@ public:
     // этот вроде бы не нужный
     Platform& operator= (const Platform& r) = delete;
     // нормальные мужыцкие методы для мужиков
+    bool collideWithPlatform(const Creature& cr);
     inline AABB getFixture() { return fixture; };
 };
 
@@ -35,11 +36,20 @@ Platform::~Platform()
 // TODO: проверить, дабловые ли здесь аргументы или интовые
 //
 
-Platform::Platform(const Platform& r):
+Platform::Platform(const Platform& r) :
     fixture(r.fixture),
     sprite(txCreateCompatibleDC(fixture.maximum.x, fixture.minimum.y))
 {
     txBitBlt(sprite, 0, 0, 0, 0, r.sprite);
+}
+
+bool Platform::collideWithPlatform(const Creature& cr) :
+{
+    if(cr.getData().get_velocity_y() > 0)
+        return false;
+    if(fixture.minimum.y > cr.get_fixture().maximum.y || fixture.minimum.x > cr.get_fixture().maximum.x || fixture.maximum.x < cr.get_fixture().minimum.x)
+        return false;
+    return true;
 }
 
 #endif // __PLATFORM_H__
