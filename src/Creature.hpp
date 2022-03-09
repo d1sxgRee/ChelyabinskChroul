bf6bae26ffa470e301c35fdc01bd1fb6a91e53f3
Roma1000-7 <82238085+Roma1000-7@users.noreply.github.com>
2022-03-09 18:45:43 +0300

    Метод (bool) взаимодействия с платформой
    Метод перенесен из класса платформ. Переменная фикстур инициализируется в теле конструктора.

--- src/Creature.hpp
+++ src/Creature.hpp
@@ -17,20 +17,24 @@ private:
     AABB fixture;
     std::map < std::string, Animation > animations;
 public:
-    Creature(Coords _coords, Data _data, AABB _fixture, std::map < std::string, Animation > _animations);
+    Creature(Coords _coords, Data _data, std::map < std::string, Animation > _animations);
 
     void addAnimation(std::string name, Animation animation);
     void moveRight();
     void moveLeft();
     void jump();
     void keys_interaction(int rkey, int lkey);
+    bool collideWithPlatform(const Platform& pl);
     Coords getCoords();
     Data getData();
     AABB get_fixture();
 };
 
-Creature::Creature(Coords _coords, Data _data, AABB _fixture, std::map < std::string, Animation > _animations) :
-    coords(_coords), data(_data), fixture(_fixture), animations(_animations) {}
+Creature::Creature(Coords _coords, Data _data, std::map < std::string, Animation > _animations) :
+    coords(_coords), data(_data), fixture(_fixture), animations(_animations)
+    {
+        AABB fixture(coords, coords.x + getSizeX(), coords.y + getSizeY());
+    }
 
 void Creature::addAnimation(std::string name, Animation animation)
 {
@@ -53,6 +57,17 @@ void Creature::moveLeft()
 void Creature::jump(Level level)
 {
     /*Please create creature jump!*/
+}
+
+bool Creature::collideWithPlatform(const Platform& pl) :
+{
+    if(getData().get_velocity_y() > 0)
+        return false;
+    if(fixture.maximum.y < pl.get_fixture().minimum.y || fixture.maximum.y > pl.get_fixture().maximum.y)
+        return false;
+    if(fixture.minimum.x > pl.get_fixture().maximum.x || fixture.maximum.x < pl.get_fixture().minimum.x)
+        return false;
+    return true;
 }
 
 Coords Creature::getCoords() { return coords; }
