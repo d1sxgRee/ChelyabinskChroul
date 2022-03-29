8dbdedfd6159b27de8d6ff863a0a8b0959cae963
d3f4u1t <87200908+C3mka21@users.noreply.github.com>
2022-03-29 21:13:01 +0300

    Add files via upload

--- tests/Menu.cpp
+++ tests/Menu.cpp
@@ -1,17 +1,27 @@
 #include "TXLib.h"
 #include "Menu.hpp"
 
+#define WINDOWX 1500
+#define WINDOWY 700
+
 int main(void)
 {
-    txCreateWindow(1500,700);
+    txCreateWindow(WINDOWX,WINDOWY);
     txSetFillColor(TX_BLACK);
     txClear();
-    // HDC background = txLoadImage("...");
-    HDC play_off = txLoadImage("playbutton.bmp");
-    HDC play_on = txLoadImage("pushed_playbutton.bmp");
-    Mouse mouse();
+    Button play({{WINDOWX/2 - 200, WINDOWY/2 - 50}, {WINDOWX/2 + 200,WINDOWY/2 + 50}}, txLoadImage("playbutton.bmp"));
+    txBegin();
 
-
+    while(true)
+    {
+        play.draw();
+        if(play.wasClicked())
+            play.getAnimation().setY(619);
+        else play.getAnimation().setY(0);
+        txSleep(100);
+        txClear();
+    }
+    txEnd();
     return 0;
 }
 
